3cd1622625b2cfdeb43e1cf733c517ef10bada6d
SierraMoiseevna <utv.header@gmail.com>
2024-02-04 19:50:49 +0300

    add brain-progression game

---
+++ bin/brain-progression.js
@@ -0,0 +1,33 @@
+import { yourAnswer, runGame } from '../src/index.js';
+
+function getRandomInt(min, max) {
+  return Math.floor(Math.random() * (max - min) + min);
+}
+
+const progression = () => {
+  const randomArrLength = getRandomInt(5, 11);
+  const randomStepProgression = getRandomInt(0, 10);
+  let startValue = getRandomInt(0, 100);
+  const progressionArr = [];
+  const randomHiddenIndex = getRandomInt(0, randomArrLength);
+
+  for (let i = 0; i < randomArrLength; i += 1) {
+    progressionArr.push(startValue);
+    startValue += randomStepProgression;
+  }
+  const removed = progressionArr.splice(randomHiddenIndex, 1, '..');
+  console.log(`Question: ${progressionArr.join(' ')}`);
+  const answer = Number(yourAnswer());
+  const correctAnswer = Number(removed);
+  if (answer === correctAnswer) {
+    console.log('Correct!');
+  } else {
+    console.log(
+      `'${answer}' is wrong answer ;(. Correct answer was '${correctAnswer}'`,
+    );
+    return false;
+  }
+  return true;
+};
+
+runGame(progression);
