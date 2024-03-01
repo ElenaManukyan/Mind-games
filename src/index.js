5776b65c9494b4d26226f320a701d7428f138955
SierraMoiseevna <utv.header@gmail.com>
2024-03-01 17:09:53 +0300

    applying destructurization on line 9 in the file index.js

--- src/index.js
+++ src/index.js
@@ -6,14 +6,13 @@ const runGame = (func, message) => {
   console.log(`Hello, ${name}!`);
   console.log(message);
   for (let i = 0; i < 3; i += 1) {
-    const res = func();
-    const [question] = res;
+    const [question, correctAnswer] = func();
     console.log(question);
     const answer = readlineSync.question('Your answer: ');
-    if (answer === res[1]) {
+    if (answer === correctAnswer) {
       console.log('Correct!');
     } else {
-      return console.log(`'${answer}' is wrong answer ;(. Correct answer was '${res[1]}'.\nLet's try again, ${name}!`);
+      return console.log(`'${answer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.\nLet's try again, ${name}!`);
     }
   }
   return console.log(`Congratulations, ${name}!`);
