c23e292138a2ff5d3381e67119568783aa11d254
SierraMoiseevna <utv.header@gmail.com>
2024-03-03 15:06:15 +0300

    change name of variables

--- bin/brain-progression.js
+++ bin/brain-progression.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 
 import runGame from '../src/index.js';
-import { progression, message } from '../src/games/brain-progression.js';
+import { changeProgression, message } from '../src/games/brain-progression.js';
 
-runGame(progression, message);
+runGame(changeProgression, message);
--- src/games/brain-progression.js
+++ src/games/brain-progression.js
@@ -1,26 +1,26 @@
 import getRandomInt from '../utils.js';
 
 const createProgression = (firstValue, countElements, step) => {
-  const progressionArr = [];
+  const progression = [];
   let startVal = firstValue;
   for (let i = 0; i < countElements; i += 1) {
-    progressionArr.push(startVal);
+    progression.push(startVal);
     startVal += step;
   }
-  return progressionArr;
+  return progression;
 };
 
-const progression = () => {
-  const randomArrLength = getRandomInt(5, 11);
+const changeProgression = () => {
+  const randomSeqLen = getRandomInt(5, 11);
   const randomStepProgression = getRandomInt(0, 10);
   const startValue = getRandomInt(0, 100);
-  const progressionArr = createProgression(startValue, randomArrLength, randomStepProgression);
-  const randomHiddenIndex = getRandomInt(0, progressionArr.length);
-  const hiddenElement = progressionArr.splice(randomHiddenIndex, 1, '..');
-  const question = `Question: ${progressionArr.join(' ')}`;
+  const sequence = createProgression(startValue, randomSeqLen, randomStepProgression);
+  const randomHiddenIndex = getRandomInt(0, sequence.length);
+  const hiddenElement = sequence.splice(randomHiddenIndex, 1, '..');
+  const question = `Question: ${sequence.join(' ')}`;
   const correctAnswer = String(hiddenElement);
   return [question, correctAnswer];
 };
 const message = 'What number is missing in the progression?';
 
-export { progression, message };
+export { changeProgression, message };
